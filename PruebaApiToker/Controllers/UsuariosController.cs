e452d937311876bab0a2d3dad1fda8c66c468eb1
Karen <Karen@PCKAREN>
2024-12-13 15:01:53 -0500

    Agregar archivos de proyecto.

---
+++ PruebaApiToker/Controllers/UsuariosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLog;
+using PruebaApiToker.Models;
+
+namespace PruebaApiToker.Controllers
+{
+    [ApiController]
+    public class UsuariosController : Controller
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static readonly List<Usuario> Usuarios = [];
+
+        [Route("RecibirUsuario")]
+        [HttpPost]
+        public IActionResult RegistrarUsuario([FromBody] Usuario usuario)
+        {
+            if (string.IsNullOrEmpty(usuario.Nombre) || string.IsNullOrEmpty(usuario.Telefono))
+            {
+                return BadRequest("Verifique datos. Faltan datos obligatorios.");
+            }
+
+            //Guardar Usuario
+            Usuarios.Add(usuario);
+
+            //Enviar Mensaje
+            Logger.Info($"Mensaje de bienvenida enviado a {usuario.Nombre} al teléfono {usuario.Telefono}");
+
+            return Ok(new { mensaje = "Datos recibidos y almacenados exitosamente." });
+        }
+    }
+}
---
+++ PruebaApiToker/Models/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace PruebaApiToker.Models
+{
+    public class Usuario
+    {
+        public required string Nombre { get; set; }
+        public required string Telefono { get; set; }
+    }
+}
